b542dff87423f9c780c1d8592de5c2dc80c26333
Twan Hopmans <twanjhopmans@hotmail.com>
2023-10-05 21:19:36 +0200

    nextjs starter

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
+      Home
+    </main>
+  )
+}
+
+export default Page
